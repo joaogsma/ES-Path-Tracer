3e6f08a142058dfd89df748f8f2a5e8c52bf1c65
João Gabriel Abreu <jgsma@cin.ufpe.br>
2016-09-30 01:57:04 -0300

    added some more detail to the kd-tree.h skeleton code

--- src/kd-tree/kd_tree.h
+++ src/kd-tree/kd_tree.h
@@ -1,39 +1,59 @@
 #ifndef __GUARD_KD_TREE_H__
 #define __GUARD_KD_TREE_H__
 
-#include "../primitives/Vector3.h"
+#include "../primitives/vector3.h"
+#include "../primitives/triangle.h"
 #include <vector>
 #include <pair>
 
-struct Region {
-    std::vector<std::pair<double, double> > bounds;
-};
+struct Region { double min_x, max_x, min_y, max_y, min_z, max_z; };
 
+// Abstract node class
 class KD_Node {
-    // TODO...
+    virtual KD_Node();
 };
 
+
+// Non-leaf nodes in the KD-Tree
+class KD_Middle_Node : KD_Node {
+public:
+    const int bound_offset;
+    const double median_value;
+
+    KD_Node* const left;
+    KD_Node* const right;
+    
+    KD_Middle_Node(int bound_offset, int median_value, KD_Node* left_child, 
+            KD_Node* right_child) : 
+            bound_offset(bound_offset), median_value(median_value),
+            left(left_child), right(right_child);
+
+    ~KD_Middle_Node();
+};
+
+
+// Leaves in the KD-Tree
 class KD_Leaf : KD_Node {
-    // TODO...
+public:
+    const Triangle tri;
+
+    KD_Leaf(Triangle triangle) : tri(triangle);
 };
 
 
 class KD_Tree {
 public:
-    KD_Tree(unsigned int dimensions);
-
     ~KD_Tree();
 
-    void add_point(Vector3 point);
-
-    void remove_point(Vector3 point);
+    static KD_Tree* build_tree(std::vector<triangle>& triangles);
 
     void search( /* TODO... */ );
 
 private:
-    KD_Node* root;
-    const unsigned int dimensions;
-    unsigned int dim_counter;
+    KD_Node* const root;
+    unsigned int dim_counter
+
+    KD_Tree() : dim_counter(0);
 };
 
 #endif
