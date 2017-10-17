b2edd13fb1ab97b092fac335f9ce50b3846ecb5f
João Gabriel Abreu <jgsma@cin.ufpe.br>
2017-10-17 19:17:25 -0300

    feat(pixel_histogram): update pixel histogram implementation

--- ES-Path-Tracer/headers/evolution-strategy/pixel_histogram.h
+++ ES-Path-Tracer/headers/evolution-strategy/pixel_histogram.h
@@ -1,7 +1,9 @@
 #ifndef __GUARD_PIXEL_HISTOGRAM_H__
 #define __GUARD_PIXEL_HISTOGRAM_H__
 
-#include <map>
+#include <unordered_map>
+
+#include "shading/color3.h"
 
 namespace es
 {
@@ -10,25 +12,26 @@ namespace es
         class Pixel_Histogram {
         public:
             // Increases counter for the specified color
-            void increase(unsigned int red, unsigned int green, unsigned int blue);
+            void increase(int red, int green, int blue);
 
             /*  Computation of the mean color based on the probabilities in the 
                 histogram. This is equivalent to the summation of (p * color),
                 for all values of color and its corresponding estimated 
                 probabilities p */
-            void mean_color(unsigned int& red, unsigned int& green, 
-                unsigned int& blue) const;
+            Color3 weighted_mean_color() const;
 
             /*  Computation of the information quantity associated with a given color. 
                 The information quantity is defined as -log(p), where p is the 
                 color's estimated probability */
-            double information_quantity(unsigned int red, unsigned int green, 
-                unsigned int blue);
+            double information_quantity(int red, int green, int blue, int distance = 1);
 
         private:
-            std::map<unsigned int, std::map<unsigned int, 
-                std::map<unsigned int, unsigned int>>> color_histogram;
-            long total_samples;
+            std::unordered_map<int, unsigned int> m_color_histogram;
+            long m_total_samples;
+
+			int to_hash_key(int red, int green, int blue) const;
+
+			Color3 from_hash_key(int key) const;
         };
     }
 }
--- ES-Path-Tracer/src/evolution-strategy/pixel_histogram.cpp
+++ ES-Path-Tracer/src/evolution-strategy/pixel_histogram.cpp
@@ -1,72 +1,82 @@
+#include <algorithm>
 #include <cmath>
-#include <map>
+#include <unordered_map>
+#include <stdexcept>
 
 #include "evolution-strategy/pixel_histogram.h"
-
-using std::log;
-using std::map;
-using std::round;
+#include "shading/color3.h"
 
 namespace es
 {
     namespace fitness
     {
-        void Pixel_Histogram::increase(unsigned int red, unsigned int green, 
-            unsigned int blue)
+        void Pixel_Histogram::increase(int red, int green, int blue)
         {
-            color_histogram[red][green][blue]++;
-            total_samples++;
+			if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255)
+				throw std::invalid_argument("invalid color");
+
+			++m_color_histogram[to_hash_key(red, green, blue)];
+            ++m_total_samples;
         }
 
-        void Pixel_Histogram::mean_color(unsigned int& red, 
-            unsigned int& green, unsigned int& blue) const
+        Color3 Pixel_Histogram::weighted_mean_color() const
         {
-            typedef map<unsigned int, map<unsigned int, map<unsigned int, unsigned int>>> red_map;
-            typedef map<unsigned int, map<unsigned int, unsigned int>> green_map;
-            typedef map<unsigned int, unsigned int> blue_map;
+			Color3 weighted_sum(0.0);
 
-            double mean_red = 0, mean_green = 0, mean_blue = 0;
+			for (const std::unordered_map<int, int>::value_type& entry : m_color_histogram)
+			{
+				const Color3& color = from_hash_key(entry.first);
+				weighted_sum += entry.second * color;
+			}
 
-            // Iterate through all colors in the histogram
-            for (red_map::const_iterator red_it = color_histogram.begin();
-                red_it != color_histogram.end(); red_it++)
+			return weighted_sum / m_total_samples;
+        }
+
+        double Pixel_Histogram::information_quantity(int red, int green, int blue, int distance)
+        {
+            if (distance <= 0)
+                throw std::invalid_argument("Distance must be positive");
+
+            int distance2 = distance * distance;
+            int total_count = 0;
+
+            int min_red = std::max(red - distance, 0);
+            int max_red = std::min(red + distance, 256) - 1;
+            int min_green = std::max(green - distance, 0);
+            int max_green = std::min(green + distance, 256) - 1;
+            int min_blue = std::max(blue - distance, 0);
+            int max_blue = std::min(blue + distance, 256) - 1;
+
+            for (int r = min_red; r <= max_red; ++r)
             {
-                unsigned int red_color = red_it->first;
-
-                for (green_map::const_iterator green_it = red_it->second.begin();
-                    green_it != red_it->second.end(); green_it++)
+                for (int g = min_green; g <= max_green; ++g)
                 {
-                    unsigned int green_color = green_it->first;
-
-                    for (blue_map::const_iterator blue_it = green_it->second.begin();
-                        blue_it != green_it->second.end(); blue_it++)
+                    for (int b = min_blue; b <= max_blue; ++b)
                     {
-                        unsigned int blue_color = blue_it->first;
-                        long double count = blue_it->second;
-
-                        // Estimated percentage of this color
-                        double probability = count / total_samples;
-
-                        // Add the color's contribution to the mean
-                        mean_red += red_color * probability;
-                        mean_green += green_color * probability;
-                        mean_blue += blue_color * probability;
+                        int dist = pow(r - red, 2) + pow(g - green, 2) + pow(b - blue, 2);
+                        if (dist <= distance2)
+                            total_count += m_color_histogram[to_hash_key(r, g, b)];
                     }
                 }
             }
-
-            red = (unsigned int) round(mean_red);
-            green = (unsigned int) round(mean_green);
-            blue = (unsigned int) round(mean_blue);
-        }
-
-        double Pixel_Histogram::information_quantity(unsigned int red, 
-            unsigned int green, unsigned int blue)
-        {
-            long double count = color_histogram[red][green][blue];
-            double probability = count / total_samples;
-
+			
+            double probability = double(total_count) / m_total_samples;
             return (-1) * log(probability);
         }
+
+		int Pixel_Histogram::to_hash_key(int red, int green, int blue) const
+		{
+			return (red * 256 * 256) + (green * 256) + blue;
+		}
+
+		Color3 Pixel_Histogram::from_hash_key(int key) const
+		{
+			int red = key / (256 * 256);
+			key %= 256 * 256;
+			int green = key / 256;
+			key %= 256;
+			int blue = key;
+			return Color3(red, green, blue);
+		}
     }
 }
